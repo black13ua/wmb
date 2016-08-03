30bcf0920606055d8f378556c8451a2aecc152a1
black <v.cherednichenko@favbet.com>
2016-08-03 11:47:38 +0300

    wmb_srv module

--- apps/wmb/src/wmb_sup.erl
+++ apps/wmb/src/wmb_sup.erl
@@ -28,7 +28,16 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
+%%%%    {ok, { {one_for_all, 0, 1}, []} }.
+    RestartStrategy = simple_one_for_one,
+    MaxRestarts = 0,
+    MaxSecondsBetweenRestarts = 1,
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+    Restart = permanent,
+    Shutdown = 5000,
+    Type = worker,
+    AChild = {'wmb_srv', {'wmb_srv', start_link, []}, Restart, Shutdown, Type, [wmb_srv]},
+    {ok, {SupFlags, [AChild]}}.
 
 %%====================================================================
 %% Internal functions
