25a2004480b95a2f815025e49b2ccd4ad30953f9
Black13.UA <black13.ua@gmail.com>
2017-02-18 16:21:23 +0200

    add API for generate pages - /api/page/n

--- apps/wmb/src/api_handler.erl
+++ apps/wmb/src/api_handler.erl
@@ -15,20 +15,26 @@ init(_Type, Req, []) ->
 handle(Req, State) ->
     {Path, Req1} = cowboy_req:path(Req),
     [_, _, APIType, APIid] = binary:split(Path, [<<"/">>], [global]),
+    APIidAtom = binary_to_integer(APIid),
     io:format("Path Elements: ~p~n", [[APIType, APIid]]),
     FilesUrlRoot = <<"/files/">>,
 
     case APIType of
         <<"albums">> ->
-            {ok, Album} = data_merger:get_album_by_albumid({album_id, binary_to_integer(APIid)}),
+            {ok, Album} = data_merger:get_album_by_albumid({album_id, APIidAtom}),
             io:format("Response from /api/albums/id: ~p~n", [Album]),
             Res = jsx:encode(Album);
+        <<"page">> ->
+            SkipAlbums = (APIidAtom * 10) - 10,
+	    {ok, Albums} = data_merger:get_albums(tpl, SkipAlbums, 10),
+            io:format("Response from /api/page/item: ~p~n", [Albums]),
+            Res = jsx:encode(Albums);
         <<"random">> ->
-            {ok, RandomTrackList} = data_merger:get_random_tracks(binary_to_integer(APIid)),
+            {ok, RandomTrackList} = data_merger:get_random_tracks(APIidAtom),
             io:format("Response from /api/random/id: ~p~n", [RandomTrackList]),
             Res = jsx:encode(RandomTrackList);
         <<"tracks">> ->
-            {ok, Track2Web} = data_merger:get_track_by_trackid({track_id, binary_to_integer(APIid)}),
+            {ok, Track2Web} = data_merger:get_track_by_trackid({track_id, APIidAtom}),
             io:format("Response from /api/tracks/id: ~p~n", [Track2Web]),
             Res = jsx:encode(Track2Web);
         _ ->
