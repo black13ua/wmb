a396d665769f451e0d0732e5c80675ec0e524f1f
d.makarov <d.makarov@favbet.com>
2017-02-20 08:54:41 +0200

    make some little improuvements

---
+++ apps/wmb/priv/www/static/webpack.dll.config.js
@@ -0,0 +1,36 @@
+const path    = require('path');
+const webpack = require('webpack');
+const config = {
+  cache: true,
+  entry: {
+    vendor: [
+      'react',
+      'redux',
+      'react-redux',
+      'jquery',
+      // 'lodash',
+      'moment',
+      'classnames',
+    ],
+  },
+  output: {
+    path: path.join(__dirname, 'dist', 'dll'),
+    filename: `dll.[name].js`,
+    library: `[name]`,
+  },
+  // module: {
+  //   loaders: [
+  //           { test: /\.json$/, loader: 'json-loader' },
+  //   ],
+  // },
+
+  plugins: [
+    new webpack.DllPlugin({
+      path: path.join(__dirname, 'dist', 'dll', `[name]-manifest.json`),
+      name: `[name]`,
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+  ],
+};
+
+module.exports = config;
