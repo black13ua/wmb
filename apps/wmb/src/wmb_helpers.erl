ce2a83378216a95d07ca71c91b0be3b63434c912
Black13.UA <black13.ua@gmail.com>
2017-03-19 17:08:35 +0200

    add some SPEC and favicon.ico

--- apps/wmb/src/wmb_helpers.erl
+++ apps/wmb/src/wmb_helpers.erl
@@ -5,6 +5,8 @@
 %%%%
 % https://erlangcentral.org/wiki/index.php/Floating_Point_Rounding
 %%%%
+-spec ceiling(number()) ->
+    integer().
 ceiling(X) when X < 0 ->
     trunc(X);
 ceiling(X) ->
@@ -14,16 +16,20 @@ ceiling(X) ->
         false -> T + 1
     end.
 
+-spec skip_ets_elements(integer(), atom()) ->
+    list() | term().
 skip_ets_elements(0, Ets) ->
     Key = ets:first(Ets),
     Key;
-skip_ets_elements(Skip, Ets) ->
+skip_ets_elements(Skip, Ets) when is_integer(Skip), is_atom(Ets) ->
     Key = ets:first(Ets),
     skip_ets_elements(Skip, Ets, Key).
 
-skip_ets_elements(0, Ets, Key) ->
+-spec skip_ets_elements(integer(), atom(), term()) ->
+    list() | term().
+skip_ets_elements(0, _Ets, Key) ->
     Key;
-skip_ets_elements(Skip, Ets, Key) ->
+skip_ets_elements(Skip, Ets, Key) when is_integer(Skip), is_atom(Ets) ->
     Next = ets:next(Ets, Key),
     case Next of
         '$end_of_table' ->
