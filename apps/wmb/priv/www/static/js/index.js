969258909d41dcb19ed396eed047638d8611103b
d.makarov <d.makarov@favbet.com>
2017-04-17 15:09:15 +0300

    build with styles

--- apps/wmb/priv/www/static/js/index.js
+++ apps/wmb/priv/www/static/js/index.js
@@ -7,9 +7,7 @@ import { AppContainer } from 'react-hot-loader';
 import App from './src/containers/app';
 import store from './src/store/store';
 
-if (__DEVELOPMENT__) { // threre will be 'extract-text-webpack-plugin' in production
-    require('../sass/main.scss'); // eslint-disable-line
-}
+require('../sass/main.scss'); // eslint-disable-line
 
 const rootEl = document.getElementById('main--container');
 
