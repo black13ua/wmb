8621a63411aede83e862571503afd70056f42a63
d.makarov <d.makarov@favbet.com>
2017-05-04 08:29:41 +0300

    add validation to search

--- apps/wmb/priv/www/static/js/src/view/right-sidebar/filters/search.js
+++ apps/wmb/priv/www/static/js/src/view/right-sidebar/filters/search.js
@@ -1,22 +1,44 @@
 import React, { PropTypes } from 'react';
+import { Button, FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
 
-const SearchView = ({ search, onSearchClick, onSearchChange }) =>
-    <article className="name">
-        <h3 className="filters-header">{ 'Search:' }</h3>
 
-        <div className="filter-name">
-            <label htmlFor="textSearch">
-                <input
-                    id          = "textSearch"
-                    placeholder = "type your favorit artist"
-                    type        = "text"
-                    value       = {search}
-                    onChange    = {onSearchChange}
-                />
-            </label>
-            <button onClick = {onSearchClick} >{ 'find it!' }</button>
-        </div>
-    </article>;
+const SearchView = ({ search, onSearchClick, onSearchChange }) => {
+    function getValidationState() {
+        const length = search.length;
+        if (length > 2) return 'success';
+        return 'warning';
+    }
+
+    return (
+        <article className="name">
+            <h3 className="filters-header">{ 'Search:' }</h3>
+
+            <form className="filter-name">
+                <FormGroup
+                    controlId="formBasicText"
+                    validationState={getValidationState()}
+                >
+                    <ControlLabel>{ 'Type your favorit artist' }</ControlLabel>
+                    <FormControl
+                        placeholder = "Type your favorit artist"
+                        type        = "text"
+                        value       = {search}
+                        onChange    = {onSearchChange}
+                    />
+                    <FormControl.Feedback />
+                    <HelpBlock>3 characters minimum</HelpBlock>
+                    <Button
+                        bsStyle  = "info"
+                        disabled = {search.length < 3}
+                        onClick  = {onSearchClick}
+                    >
+                        { 'Submit' }
+                    </Button>
+                </FormGroup>
+            </form>
+        </article>
+    );
+};
 
 
 SearchView.propTypes = {
