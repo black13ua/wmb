9b371713971cc66700a438a8a7e0b7839fde653a
Black13.ua <black13.ua@gmail.com>
2017-05-12 10:23:10 +0300

    add init_ets_counters Fun

--- apps/wmb/include/ets_names.hrl
+++ apps/wmb/include/ets_names.hrl
@@ -3,6 +3,7 @@
 -define(ETS_ALBUMS, wmb_albums).
 -define(ETS_ARTISTS, wmb_artists).
 -define(ETS_COUNTERS, wmb_counters).
+-define(ETS_COVERS, wmb_covers).
 -define(ETS_PATHS,  wmb_paths).
 -define(ETS_TRACKS, wmb_tracks).
 -define(ETS_ERRORS, wmb_errors).
